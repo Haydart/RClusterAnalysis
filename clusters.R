e2257878d4a5c27ef0d4377108bb404864a44c63
Haydart <makowiecki.radoslaw@gmail.com>
2018-05-21 20:32:54 +0200

    Refactor code

--- clusters.R
+++ clusters.R
@@ -2,23 +2,41 @@ library(cluster)
 library(fpc)
 library(clusterSim)
 library(clusterCrit)
+library(colorspace)
 
 
 ClusterPurity <- function(clusters, classes) {
   sum(apply(table(classes, clusters), 2, max)) / length(clusters)
 }
 
-data <- read.csv("datasets/glass.csv")
+data <- read.csv("datasets/wine.csv")
+labels <- data[, c("class")]
+data <- data[, -which(names(data) %in% c("class"))]
 data <- data.Normalization(data, type="n1", normalization="column")
 data
 # Kmeans cluster analysis
-clusters <- kmeans(data[, -which(names(data) %in% c("class"))], centers=6)
+clusters <- kmeans(data, centers=6)
 #clusters = pam(data[, -1], centers=5)
 
-plot(data[, c(4, 5)], col = clusters$cluster + 1, lty = "solid", pch = 19, cex=0)
-text(data[, c(4, 5)], labels=as.numeric(data[, c("class")]), cex=1, col=clusters$cluster + 1)
+#plot(data[, c(4, 5)], col = clusters$cluster + 1, lty = "solid", pch = 19, cex=0)
+#text(data[, c(4, 5)], labels=as.numeric(data[, c("class")]), cex=1, col=clusters$cluster + 1)
 
-intCriteria(as.matrix(sapply(data[, 1:7], as.numeric)), clusters$cluster, c("Dunn", "Davies_Bouldin", "Silhouette"))
+#data2 <- data[, -which(names(data) %in% c("class"))]
+#labels <- iris[,5]
+#species_col <- rev(rainbow_hcl(3))[as.numeric(labels)]
+
+#pairs(iris2, col = species_col,
+#      lower.panel = NULL,
+#      cex.labels=2, pch=19, cex = 1.2)
+
+# Add a legend
+#par(xpd = TRUE)
+#legend(x = 0.05, y = 0.4, cex = 2,
+#       legend = as.character(levels(labels)),
+#       fill = unique(species_col))
+#par(xpd = NA)
+
+intCriteria(as.matrix(sapply(data[, -which(names(data) %in% c("class"))], as.numeric)), clusters$cluster, c("Dunn", "Davies_Bouldin", "Silhouette"))
 
 ClusterPurity(clusters$cluster, data[, c("class")])
 
