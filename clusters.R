c8d4bcabd8b3f18de3cd3b866fb7352be848b423
Haydart <makowiecki.radoslaw@gmail.com>
2018-05-22 01:26:29 +0200

    Clusterize in for loop over different K

--- clusters.R
+++ clusters.R
@@ -4,42 +4,32 @@ library(clusterSim)
 library(clusterCrit)
 library(colorspace)
 
-CLUSTER_COUNT = 10
+MIN_CLUSTER_COUNT = 2
+MAX_CLUSTER_COUNT = 10
 
-ClusterPurity <- function(clusters, classes) {
+clusterPurity <- function(clusters, classes) {
   sum(apply(table(classes, clusters), 2, max)) / length(clusters)
 }
 
-data <- read.csv("datasets/seeds.csv")
-data
+plotDataPairs <- function(class_column) {
+  pairs(data, col=class_column, lower.panel=NULL, cex.labels=2, pch=19, cex=1.2)
+}
+
+data <- read.csv("datasets/wine.csv")
 labels <- data[, c("class")]
 data <- data[, -which(names(data) %in% c("class"))]
 data <- data.Normalization(data, type="n1", normalization="column")
-data
 
-# Kmeans cluster analysis
-clusters <- kmeans(data, centers=CLUSTER_COUNT)
-#clusters = pam(data[, -1], centers=5)
-
-species_col <- rev(rainbow_hcl(CLUSTER_COUNT))[as.numeric(clusters$cluster)]
-species_col2 <- rev(rainbow_hcl(CLUSTER_COUNT))[as.numeric(labels)]
-
-pairs(data, col=species_col,
-      lower.panel=NULL,
-      cex.labels=2, pch=19, cex=1.2)
-
-pairs(data, col=species_col2,
-      lower.panel=NULL,
-      cex.labels=2, pch=19, cex=1.2)
-
-intCriteria(as.matrix(sapply(data, as.numeric)), clusters$cluster, c("Dunn", "Davies_Bouldin", "Silhouette"))
-
-ClusterPurity(clusters$cluster, labels)
-
-#data <- read.csv("seeds.data")
-# Kmeans clustre analysis
-#clus <- kmeans(data[, -8], centers=3)
-#clus = pam(data[, -8], centers=3)
-
-#plot(data[, c(2, 3)], col = clus$cluster + 1, lty = "solid", pch = 19, cex=0)
-#text(data[, c(2, 3)], labels=as.numeric(data[, 8]), cex=1, col=clus$cluster + 1)
+for(clusters_count in MIN_CLUSTER_COUNT:MAX_CLUSTER_COUNT) {
+  print("")
+  clusters <- kmeans(data, centers=clusters_count)
+  
+  cluster_col <- rev(rainbow_hcl(clusters_count))[as.numeric(clusters$cluster)]
+  real_col <- rev(rainbow_hcl(clusters_count))[as.numeric(labels)]
+  
+  plotDataPairs(cluster_col)
+  plotDataPairs(real_col)
+  
+  print(intCriteria(as.matrix(sapply(data, as.numeric)), clusters$cluster, c("Dunn", "Davies_Bouldin", "Silhouette")))
+  print(clusterPurity(clusters$cluster, labels))
+}
