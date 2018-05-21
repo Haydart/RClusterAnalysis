dd9318030c91d262f895a7d364cc1bfc5045f2df
Haydart <makowiecki.radoslaw@gmail.com>
2018-05-21 20:35:09 +0200

    Display pair grid for clusters

--- clusters.R
+++ clusters.R
@@ -21,24 +21,21 @@ clusters <- kmeans(data, centers=6)
 #plot(data[, c(4, 5)], col = clusters$cluster + 1, lty = "solid", pch = 19, cex=0)
 #text(data[, c(4, 5)], labels=as.numeric(data[, c("class")]), cex=1, col=clusters$cluster + 1)
 
-#data2 <- data[, -which(names(data) %in% c("class"))]
-#labels <- iris[,5]
-#species_col <- rev(rainbow_hcl(3))[as.numeric(labels)]
+species_col <- rev(rainbow_hcl(3))[as.numeric(labels)]
 
-#pairs(iris2, col = species_col,
-#      lower.panel = NULL,
-#      cex.labels=2, pch=19, cex = 1.2)
+pairs(data, col=species_col,
+      lower.panel=NULL,
+      cex.labels=2, pch=19, cex=1.2)
 
-# Add a legend
-#par(xpd = TRUE)
-#legend(x = 0.05, y = 0.4, cex = 2,
-#       legend = as.character(levels(labels)),
-#       fill = unique(species_col))
-#par(xpd = NA)
+par(xpd=TRUE)
+legend(x=0.05, y = 0.4, cex=2,
+       legend = as.character(levels(labels)),
+       fill = unique(species_col))
+par(xpd=NA)
 
-intCriteria(as.matrix(sapply(data[, -which(names(data) %in% c("class"))], as.numeric)), clusters$cluster, c("Dunn", "Davies_Bouldin", "Silhouette"))
+intCriteria(as.matrix(sapply(data, as.numeric)), clusters$cluster, c("Dunn", "Davies_Bouldin", "Silhouette"))
 
-ClusterPurity(clusters$cluster, data[, c("class")])
+ClusterPurity(clusters$cluster, labels)
 
 #data <- read.csv("seeds.data")
 # Kmeans clustre analysis
